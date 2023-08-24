ac54e38aaaa3bc1cee73c4a09552ae8744bf96ff
mifa2022@outlook.com <mifa2022@outlook.com>
2023-08-24 09:11:21 +0200

    initial commit

---
+++ maui-flyout-open-issue/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlyoutOpen_Clicked(object sender, EventArgs e)
+        {
+            Shell.Current.FlyoutIsPresented = true;
+        }
+    }
+}
